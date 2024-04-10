bf37ee1c6bcc6c6d7a305087c89fde2fd31027ef
Vladyslav <v1adem@ukr.net>
2024-04-10 23:23:13 +0300

    Added and implemented PatientService

---
+++ src/main/java/com/v1adem/polyclinic/service/PatientService.java
@@ -0,0 +1,40 @@
+package com.v1adem.polyclinic.service;
+
+import com.v1adem.polyclinic.entity.Patient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class PatientService {
+    private final PatientRepository patientRepository;
+
+    public void addPatient(Patient patient) {
+        patientRepository.save(patient);
+    }
+
+    public Patient findById(long id) {
+        Optional<Patient> patientOptional = patientRepository.findById(id);
+        return patientOptional.orElse(null);
+    }
+
+    public List<Patient> findAll() {
+        return patientRepository.findAll().stream().toList();
+    }
+
+    public boolean deletePatientById(Long id) {
+        Optional<Patient> patientOptional = patientRepository.findById(id);
+        if (patientOptional.isPresent()) {
+            patientRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    public long getTotalNumberOfPatients() {
+        return patientRepository.count();
+    }
+}
