64a9fa3c3aa417d5050f80eb512b8ff16a5fdf0b
Vladyslav <v1adem@ukr.net>
2024-04-14 18:04:46 +0300

    Added and implemented VisitService

---
+++ src/main/java/com/v1adem/polyclinic/service/VisitService.java
@@ -0,0 +1,42 @@
+package com.v1adem.polyclinic.service;
+
+import com.v1adem.polyclinic.entity.Patient;
+import com.v1adem.polyclinic.entity.Visit;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public void addVisit(Visit visit) {
+        visitRepository.save(visit);
+    }
+
+    public Visit findById(long id) {
+        Optional<Visit> patientOptional = visitRepository.findById(id);
+        return patientOptional.orElse(null);
+    }
+
+    public List<Visit> findAll() {
+        return visitRepository.findAll().stream().toList();
+    }
+
+    public boolean deleteVisitById(Long id) {
+        Optional<Visit> patientOptional = visitRepository.findById(id);
+        if (patientOptional.isPresent()) {
+            visitRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    public long getTotalNumberOfVisits() {
+        return visitRepository.count();
+    }
+}
