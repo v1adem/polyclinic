fb13ccacea09534102a267a4013a820ad8d94cca
Vladyslav <v1adem@ukr.net>
2024-05-10 09:45:31 +0300

    fixed bug with Beans

---
+++ src/main/java/com/v1adem/polyclinic/MongoDBConfiguration.java
@@ -0,0 +1,22 @@
+package com.v1adem.polyclinic;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class MongoDBConfiguration {
+
+
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create(System.getenv("MONGODB_URI"));
+    }
+    @Bean
+    public MongoOperations mongoTemplate() {
+        return new MongoTemplate(mongoClient(), "polyclinic");
+    }
+}
